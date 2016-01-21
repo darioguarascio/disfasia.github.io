dbb24abd8b27d31c7c435165d800bf9ee53e0806
Dario <me@disf.asia>
2016-01-21 08:35:06 +0100

    general improvements

--- js/main.js
+++ js/main.js
@@ -18,160 +18,304 @@ $(document).ready(function() {
     });
     */
 
-    setTimeout(1000,function() {
-        var myLatLng = new google.maps.LatLng( 50, 50 ),
-            myOptions = {
-                streetViewControl: false,
-                zoomControl: false,
-                scaleControl: false,
-                mapTypeControl: false,
-                zoom: 4,
-                center: myLatLng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-                },
-            map = new google.maps.Map( $('#travelmap')[0], myOptions ),
-            marker = new google.maps.Marker( {position: myLatLng, map: map} );
+    // setTimeout(1000,function() {
+    //     var myLatLng = new google.maps.LatLng( 50, 50 ),
+    //         myOptions = {
+    //             streetViewControl: false,
+    //             zoomControl: false,
+    //             scaleControl: false,
+    //             mapTypeControl: false,
+    //             zoom: 4,
+    //             center: myLatLng,
+    //             mapTypeId: google.maps.MapTypeId.ROADMAP
+    //             },
+    //         map = new google.maps.Map( $('#travelmap')[0], myOptions ),
+    //         marker = new google.maps.Marker( {position: myLatLng, map: map} );
         
-        marker.setMap( map );        
-    });
-
-
-
-
-
+    //     marker.setMap( map );        
+    // });
 });
-/*
-jQuery(document).ready(function($) {
-        var gallery = $('#thumbs').galleriffic({
-        delay:                     3000, // in milliseconds
-        numThumbs:                 20, // The number of thumbnails to show page
-        preloadAhead:              40, // Set to -1 to preload all images
-        enableTopPager:            false,
-        enableBottomPager:         true,
-        maxPagesToShow:            7,  // The maximum number of pages to display in either the top or bottom pager
-        imageContainerSel:         '#slideshow', // The CSS selector for the element within which the main slideshow image should be rendered
-        controlsContainerSel:      '#controls', // The CSS selector for the element within which the slideshow controls should be rendered
-        captionContainerSel:       '#caption', // The CSS selector for the element within which the captions should be rendered
-        loadingContainerSel:       '#loading', // The CSS selector for the element within which should be shown when an image is loading
-        renderSSControls:          true, // Specifies whether the slideshow's Play and Pause links should be rendered
-        renderNavControls:         true, // Specifies whether the slideshow's Next and Previous links should be rendered
-        playLinkText:              'Play',
-        pauseLinkText:             'Pause',
-        prevLinkText:              'Previous',
-        nextLinkText:              'Next',
-        nextPageLinkText:          'Next &rsaquo;',
-        prevPageLinkText:          '&lsaquo; Prev',
-        enableHistory:             false, // Specifies whether the url's hash and the browser's history cache should update when the current slideshow image changes
-        enableKeyboardNavigation:  true, // Specifies whether keyboard navigation is enabled
-        autoStart:                 false, // Specifies whether the slideshow should be playing or paused when the page first loads
-        syncTransitions:           false, // Specifies whether the out and in transitions occur simultaneously or distinctly
-        defaultTransitionDuration: 500, // If using the default transitions, specifies the duration of the transitions
-        onSlideChange:             undefined, // accepts a delegate like such: function(prevIndex, nextIndex) { ... }
-        onTransitionOut:           undefined, // accepts a delegate like such: function(slide, caption, isSync, callback) { ... }
-        onTransitionIn:            undefined, // accepts a delegate like such: function(slide, caption, isSync) { ... }
-        onPageTransitionOut:       undefined, // accepts a delegate like such: function(callback) { ... }
-        onPageTransitionIn:        undefined, // accepts a delegate like such: function() { ... }
-        onImageAdded:              undefined, // accepts a delegate like such: function(imageData, $li) { ... }
-        onImageRemoved:            undefined  // accepts a delegate like such: function(imageData, $li) { ... }
-    });
-});
-*/
- $(function(){
-        var lat = $('#world-map-markers').data('lat'), lng = $('#world-map-markers').data('lng'), country = $('#world-map-markers').data('country');
-        var region_values = {}; region_values[country] = '#D5E4CA';
+
+$(function() {
+    if ($('body#t_album').size()) {
+
+        var lat = $('#world-map-markers').data('lat'),
+            lng = $('#world-map-markers').data('lng'),
+            country = $('#world-map-markers').data('country');
+        var region_values = {};
+        region_values[country] = '#D5E4CA';
         $('#world-map-markers').vectorMap({
-           zoomMax: 22,
-          //zoomOnScroll: false,
-          //zoomMin: 3,
-          //panOnDrag: false,
-          zoomButtons : false,
+            zoomMax: 22,
+            zoomMin: 5,
+            panOnDrag: false,
+            //zoomOnScroll: false,
+            //panOnDrag: false,
+            zoomButtons: false,
 
-onRegionLabelShow: function(e, el, code){
-    e.preventDefault();
-},
-          onRegionTipShow: function(e, el, code){
-            e.preventDefault();
-          },
-  onMarkerTipShow: function(e, label, code){
-    e.preventDefault();
-    //label.html('Anything you want');
-    //or do what you want with label, it's just a jQuery object
-  },          
-          map: 'world_mill_en',
-          scaleColors: ['#C8EEFF', '#0071A4'],
-          normalizeFunction: 'polynomial',
-          hoverOpacity: 0.7,
-          hoverColor: false,
-          markerStyle: {
-            initial: {
-                fill: '#f4a460',
-                stroke: '#383f47',
-                "fill-opacity": 1,
-                "stroke-width": 1,
-                "stroke-opacity": 1,
-                r: 5
-            }
-          },
-regionStyle: {
-      initial: {
-        fill: '#A0AB98'
-      },
-      selected: {
-        fill: '#A0AB98'
-      },
-     hover: {
-         "fill-opacity": 1
-     }
-    },
-          focusOn: {
-lat: lat,
-lng: lng,
-scale: 12
-},
+            onRegionLabelShow: function(e, el, code) {
+                e.preventDefault();
+            },
+            onRegionTipShow: function(e, el, code) {
+                e.preventDefault();
+            },
+            onMarkerTipShow: function(e, label, code) {
+                e.preventDefault();
+            },
+            map: 'world_mill_en',
+            scaleColors: ['#C8EEFF', '#0071A4'],
+            normalizeFunction: 'polynomial',
+            hoverOpacity: 0.7,
+            hoverColor: false,
+            markerStyle: {
+                initial: {
+                    fill: '#f4a460',
+                    stroke: '#383f47',
+                    "fill-opacity": 1,
+                    "stroke-width": 1,
+                    "stroke-opacity": 1,
+                    r: 5
+                }
+            },
+            regionStyle: {
+                initial: {
+                    fill: '#A0AB98'
+                },
+                selected: {
+                    fill: '#A0AB98'
+                },
+                hover: {
+                    "fill-opacity": 1
+                }
+            },
+            focusOn: {
+                lat: lat,
+                lng: lng,
+                scale: 12
+            },
 
 
-series: {
-    // regions: [{
-    //         scale: {
-    //             '1': '#4169E1'
-    //         },
-    //         attribute: 'fill',
-    //         values: { 'BE': '1', 'NL': '1', 'DE': '1' }
-    //     }] 
-    regions: [{
-        values: region_values,
-        attribute: 'fill'
-    }] 
-    },
-          backgroundColor: '#383f47',
-          markers: [
-            {latLng: [lat, lng] },
-            // {latLng: [43.73, 7.41], name: 'Monaco'},
-            // {latLng: [-0.52, 166.93], name: 'Nauru'},
-            // {latLng: [-8.51, 179.21], name: 'Tuvalu'},
-            // {latLng: [43.93, 12.46], name: 'San Marino'},
-            // {latLng: [47.14, 9.52], name: 'Liechtenstein'},
-            // {latLng: [7.11, 171.06], name: 'Marshall Islands'},
-            // {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
-            // {latLng: [3.2, 73.22], name: 'Maldives'},
-            // {latLng: [35.88, 14.5], name: 'Malta'},
-            // {latLng: [12.05, -61.75], name: 'Grenada'},
-            // {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
-            // {latLng: [13.16, -59.55], name: 'Barbados'},
-            // {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
-            // {latLng: [-4.61, 55.45], name: 'Seychelles'},
-            // {latLng: [7.35, 134.46], name: 'Palau'},
-            // {latLng: [42.5, 1.51], name: 'Andorra'},
-            // {latLng: [14.01, -60.98], name: 'Saint Lucia'},
-            // {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
-            // {latLng: [1.3, 103.8], name: 'Singapore'},
-            // {latLng: [1.46, 173.03], name: 'Kiribati'},
-            // {latLng: [-21.13, -175.2], name: 'Tonga'},
-            // {latLng: [15.3, -61.38], name: 'Dominica'},
-            // {latLng: [-20.2, 57.5], name: 'Mauritius'},
-            // {latLng: [26.02, 50.55], name: 'Bahrain'},
-            // {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
-          ]
+            series: {
+                regions: [{
+                    values: region_values,
+                    attribute: 'fill'
+                }]
+            },
+            backgroundColor: '#383f47',
+            markers: [{
+                latLng: [lat, lng]
+            }, ]
         });
 
-      })
+        var initPhotoSwipeFromDOM = function(gallerySelector) {
+
+            // parse slide data (url, title, size ...) from DOM elements 
+            // (children of gallerySelector)
+            var parseThumbnailElements = function(el) {
+                var thumbElements = el.childNodes,
+                    numNodes = thumbElements.length,
+                    items = [],
+                    figureEl,
+                    linkEl,
+                    size,
+                    item;
+
+                for(var i = 0; i < numNodes; i++) {
+
+                    figureEl = thumbElements[i]; // <figure> element
+
+                    // include only element nodes 
+                    if(figureEl.nodeType !== 1) {
+                        continue;
+                    }
+
+                    linkEl = figureEl.children[0]; // <a> element
+
+                    size = linkEl.getAttribute('data-size').split('x');
+
+                    // create slide object
+                    item = {
+                        src: linkEl.getAttribute('href'),
+                        w: parseInt(size[0], 10),
+                        h: parseInt(size[1], 10)
+                    };
+
+
+
+                    if(figureEl.children.length > 1) {
+                        // <figcaption> content
+                        item.title = figureEl.children[1].innerHTML; 
+                    }
+
+                    if(linkEl.children.length > 0) {
+                        // <img> thumbnail element, retrieving thumbnail url
+                        item.msrc = linkEl.children[0].getAttribute('src');
+                    } 
+
+                    item.el = figureEl; // save link to element for getThumbBoundsFn
+                    items.push(item);
+                }
+
+                return items;
+            };
+
+            // find nearest parent element
+            var closest = function closest(el, fn) {
+                return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+            };
+
+            // triggers when user clicks on thumbnail
+            var onThumbnailsClick = function(e) {
+                e = e || window.event;
+                e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+                var eTarget = e.target || e.srcElement;
+
+                // find root element of slide
+                var clickedListItem = closest(eTarget, function(el) {
+                    return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+                });
+
+                if(!clickedListItem) {
+                    return;
+                }
+
+                // find index of clicked item by looping through all child nodes
+                // alternatively, you may define index via data- attribute
+                var clickedGallery = clickedListItem.parentNode,
+                    childNodes = clickedListItem.parentNode.childNodes,
+                    numChildNodes = childNodes.length,
+                    nodeIndex = 0,
+                    index;
+
+                for (var i = 0; i < numChildNodes; i++) {
+                    if(childNodes[i].nodeType !== 1) { 
+                        continue; 
+                    }
+
+                    if(childNodes[i] === clickedListItem) {
+                        index = nodeIndex;
+                        break;
+                    }
+                    nodeIndex++;
+                }
+
+
+
+                if(index >= 0) {
+                    // open PhotoSwipe if valid index found
+                    openPhotoSwipe( index, clickedGallery );
+                }
+                return false;
+            };
+
+            // parse picture index and gallery index from URL (#&pid=1&gid=2)
+            var photoswipeParseHash = function() {
+                var hash = window.location.hash.substring(1),
+                params = {};
+
+                if(hash.length < 5) {
+                    return params;
+                }
+
+                var vars = hash.split('&');
+                for (var i = 0; i < vars.length; i++) {
+                    if(!vars[i]) {
+                        continue;
+                    }
+                    var pair = vars[i].split('=');  
+                    if(pair.length < 2) {
+                        continue;
+                    }           
+                    params[pair[0]] = pair[1];
+                }
+
+                if(params.gid) {
+                    params.gid = parseInt(params.gid, 10);
+                }
+
+                return params;
+            };
+
+            var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+                var pswpElement = document.querySelectorAll('.pswp')[0],
+                    gallery,
+                    options,
+                    items;
+
+                items = parseThumbnailElements(galleryElement);
+
+                // define options (if needed)
+                options = {
+                    history: false,
+                    // define gallery index (for URL)
+                    galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+                    getThumbBoundsFn: function(index) {
+                        // See Options -> getThumbBoundsFn section of documentation for more info
+                        var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                            pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                            rect = thumbnail.getBoundingClientRect(); 
+
+                        return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+                    }
+
+                };
+
+                // PhotoSwipe opened from URL
+                if(fromURL) {
+                    if(options.galleryPIDs) {
+                        // parse real index when custom PIDs are used 
+                        // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                        for(var j = 0; j < items.length; j++) {
+                            if(items[j].pid == index) {
+                                options.index = j;
+                                break;
+                            }
+                        }
+                    } else {
+                        // in URL indexes start from 1
+                        options.index = parseInt(index, 10) - 1;
+                    }
+                } else {
+                    options.index = parseInt(index, 10);
+                }
+
+                // exit if index not found
+                if( isNaN(options.index) ) {
+                    return;
+                }
+
+                if(disableAnimation) {
+                    options.showAnimationDuration = 0;
+                }
+
+                // Pass data to PhotoSwipe and initialize it
+                gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+                gallery.init();
+            };
+
+            // loop through all gallery elements and bind events
+            var galleryElements = document.querySelectorAll( gallerySelector );
+
+            for(var i = 0, l = galleryElements.length; i < l; i++) {
+                galleryElements[i].setAttribute('data-pswp-uid', i+1);
+                galleryElements[i].onclick = onThumbnailsClick;
+            }
+
+            // Parse URL and open gallery if it contains #&pid=3&gid=1
+            var hashData = photoswipeParseHash();
+            if(hashData.pid && hashData.gid) {
+                openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+            }
+        };
+
+        // execute above function
+        initPhotoSwipeFromDOM('.my-gallery');
+
+
+
+
+
+
+    }
+});
